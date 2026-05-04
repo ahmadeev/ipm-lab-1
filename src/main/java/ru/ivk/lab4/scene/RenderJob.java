9074d3a863f2b690a0a4498a3e0f363240b5a47b
danis <ahrnadeev@gmail.com>
2026-05-04 19:31:38 +0300

    feat(lab4): add native png output

--- src/main/java/ru/ivk/lab4/scene/RenderJob.java
+++ src/main/java/ru/ivk/lab4/scene/RenderJob.java
@@ -1,21 +1,12 @@
 package ru.ivk.lab4.scene;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import ru.ivk.lab4.core.Camera;
 
+@Getter
+@RequiredArgsConstructor
 public final class RenderJob {
     private final Scene scene;
     private final Camera camera;
-
-    public RenderJob(Scene scene, Camera camera) {
-        this.scene = scene;
-        this.camera = camera;
-    }
-
-    public Scene getScene() {
-        return scene;
-    }
-
-    public Camera getCamera() {
-        return camera;
-    }
 }
